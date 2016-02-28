15971c8cbb6bf1e2cdd496c234a1698e74d40705
Oasis <Fleeting>
2016-02-28 04:16:18 -0600

    finalizing design

--- core/resources/views/pages/home.blade.php
+++ core/resources/views/pages/home.blade.php
@@ -1,92 +1,78 @@
 <section class="slider">
-<div class="container">
-	<div class="row">
-		<div class="col-md-7">
-			<div class="overview">
-				<h2 class="title animated fadeInUp delayp1" style="opacity: 0;">SNT Media Laravel-At-Rest Job Application</h2>
-				<ul class="summary animated fadeInUp delayp2" style="opacity: 0;">
-					<li><i class="fa fa-cog fa-spin"></i> Consuming xml data... </li>
-					<li><i class="fa fa-cog fa-spin"></i> Fetching... </li>
-					<li><i class="fa fa-cog fa-spin"></i> Parsing.... </li>
-					<li><i class="fa fa-cog fa-spin"></i> Storing.... </li>
-				</ul>
-			</div>	
-		</div>
-		
-		<div id="consolewrapper" class="col-md-5">
-            <div class="container">
-                <div class="console">
-                    <div class="line white">Welcome to cssConsole! Type help for command list.</div>
-                </div>
-                <div class="bottom">
-                    <div class="label"></div>
-                    <div id="input"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="overview">
+                    <h2 class="title animated fadeInUp delayp1" style="opacity: 0;">Laravel-At-Rest</h2>
+                    <ul class="summary animated fadeInUp delayp2" style="opacity: 0;">
+                        <li><i class="fa fa-cog fa-spin"></i> Consuming xml data...</li>
+                        <li><i class="fa fa-cog fa-spin"></i> Fetching...</li>
+                        <li><i class="fa fa-cog fa-spin"></i> Parsing....</li>
+                        <li><i class="fa fa-cog fa-spin"></i> Storing....</li>
+                    </ul>
                 </div>
             </div>
-		</div>
-	</div>
-</div>
+            <div id="consolewrapper" class="col-md-8">
+                <section>
+                    <div id="terminal" class="cmd_terminal_theme_fallout"></div>
+                </section>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <div class="row">
+            <br/>
+            <div class="col-md-3 well center-block">
+                <span class="btn btn-danger btn-lg btn-block">GET <i class="fa fa-retweet">&nbsp;</i></span>
+            </div>
+            <div class="col-md-3 well center-block">
+                <span class="btn btn-danger btn-lg btn-block"><i class="fa fa-share-alt">&nbsp;</i> POST </span>
+            </div>
+            <div class="col-md-3 well center-block">
+                <span class="btn btn-danger btn-lg btn-block">PUT/PATCH <i class="fa fa-toggle-on">&nbsp;</i></span>
+            </div>
+            <div class="col-md-3 well center-block">
+                <span class="btn btn-danger btn-lg btn-block"><i class="fa fa-toggle-off">&nbsp;</i> DELETE </span>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
 
 </section>
-
-
-<div class="social-buttons">
-  <div class="container">
-    <ul class="list-inline">
-        <span class="btn-danger btn-lg spacing"> Lyle Mcclanahan <i class="fa fa-anchor">&nbsp;</i></span>
-        <span>&nbsp;&nbsp;</span>
-        <span class="btn-danger btn-lg span5"><i class="fa fa-anchor">&nbsp;</i> Silicon-Prairie.Net </span>
-    </ul>
-  </div>
-</div>
-
-
 <div class="container features  clearfix" style="display:none;">
-	<div class="row">
-
+    <div class="row">
         <div class="col-md-4">
-            <div class="feature-box">
-                <div class="box-media">
-
-                </div>
-                <div class="box-desc">
-
-                </div>
+            <div class="box-media">
+            </div>
+            <div class="box-desc">
             </div>
         </div>
-
-        <div class="col-md-4">
-            <div class="feature-box media-box">
-                <div class="box-media">
-
-                </div>
-                <div class="box-desc">
-
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="feature-box media-box">
-                <div class="box-media">
-
-                </div>
-                <div class="box-desc">
-
-                </div>
-            </div>
-        </div>
-
-        <div class="clear"></div>
-
     </div>
-</div>    
-
-
+    <div class="col-md-4">
+        <div class="feature-box media-box">
+            <div class="box-media">
+            </div>
+            <div class="box-desc">
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="feature-box media-box">
+            <div class="box-media">
+            </div>
+            <div class="box-desc">
+            </div>
+        </div>
+    </div>
+    <div class="clear"></div>
+</div>
+</div>
 <section class="text-promo" style="padding: 100px 0px; background-position: 50% 5.40498px;" class="section parallax bottommargin-lg">
     <div class="container text-center">
-	    <div class="heading-block center nobottomborder nobottommargin">
-	        <h2> <?php echo \Illuminate\Foundation\Inspiring::quote(); ?> </h2>
-	    </div>
-	</div>    
+        <div class="heading-block center nobottomborder nobottommargin">
+            <h2> <?php echo \Illuminate\Foundation\Inspiring::quote(); ?> </h2>
+        </div>
+    </div>
 </section>
